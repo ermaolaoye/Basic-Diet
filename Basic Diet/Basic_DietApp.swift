140dcf326d7032cf0cb00a60e815c8ae5358d026
ermaolaoye <ermaolaoye233@gmail.com>
2020-09-26 16:39:08 +0800

    Initial Commit

---
+++ Basic Diet/Basic_DietApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_DietApp.swift
+//  Basic Diet
+//
+//  Created by 黄子航 on 2020/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Basic_DietApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Basic Diet/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Basic Diet
+//
+//  Created by 黄子航 on 2020/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
